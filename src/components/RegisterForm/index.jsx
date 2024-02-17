eddbdbc0bc3b38df953f206b881dc07b8c7cd7c9
saibia8 <sabina.kutniauske@gmail.com>
2024-02-17 13:54:55 +0100

    Finished register form

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -17,8 +17,10 @@ const RegisterForm = () => {
   const formik = useFormik({
     initialValues: {
       name: '',
-      email: '',
+      emailId: '',
       password: '',
+      avatar: '',
+      venueManager: false,
     },
     validationSchema: Yup.object({
       name: Yup.string()
@@ -27,7 +29,7 @@ const RegisterForm = () => {
           'Invalid name. Only alphanumeric characters are allowed. For example: "John123"'
         )
         .required('Required'),
-      email: Yup.string()
+      emailId: Yup.string()
         .matches(
           /^[\w\-.]+@(stud\.)?noroff\.no$/i,
           'Invalid email address. Please follow this pattern: "name@noroff.no" or "name@stud.noroff.no"'
@@ -36,58 +38,112 @@ const RegisterForm = () => {
       password: Yup.string()
         .min(8, 'Must be 8 characters or more')
         .required('Required'),
+      avatar: Yup.string().url('Invalid URL'),
+      venueManager: Yup.boolean(),
     }),
     onSubmit: (values) => {
-      console.log(values);
-      registerUserMutation.mutate(values);
+      const data = {
+        name: values.name,
+        email: values.emailId,
+        password: values.password,
+        avatar: values.avatar,
+        venueManager: values.venueManager,
+      };
+      console.log(data);
+      registerUserMutation.mutate(data);
     },
   });
 
   return (
-    <form
-      className='flex flex-col w-1/2 mx-auto space-y-2'
-      onSubmit={formik.handleSubmit}
-    >
-      <label htmlFor='name'>Name:</label>
-      <input
-        className='border border-gray-400 rounded p-1'
-        type='text'
-        id='name'
-        name='name'
-        onChange={formik.handleChange}
-        value={formik.values.name}
-      />
-      {formik.touched.name && formik.errors.name ? (
-        <div>{formik.errors.name}</div>
-      ) : null}
+    <div className='bg-green block max-w-md rounded-lg p-6'>
+      <form
+        className='flex flex-col w-5/6 mx-auto space-y-3 '
+        onSubmit={formik.handleSubmit}
+      >
+        <label htmlFor='name' className='font-bold mt-4'>
+          Name:
+        </label>
+        <input
+          className='border text-primary border-gray-400 rounded p-1'
+          type='text'
+          id='name'
+          name='name'
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div className='text-orange'>{formik.errors.name}</div>
+        ) : null}
 
-      <label htmlFor='email'>Email:</label>
-      <input
-        className='border border-gray-400 rounded p-1'
-        type='email'
-        id='email'
-        name='email'
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
+        <label htmlFor='emailId' className='font-bold mt-4'>
+          Email:
+        </label>
+        <input
+          className='border text-primary border-gray-400 rounded p-1'
+          type='email'
+          id='emailId'
+          name='emailId'
+          onChange={formik.handleChange}
+          value={formik.values.emailId}
+        />
+        {formik.touched.emailId && formik.errors.emailId ? (
+          <div className='text-orange'>{formik.errors.emailId}</div>
+        ) : null}
 
-      <label htmlFor='password'>Password:</label>
-      <input
-        className='border border-gray-400 rounded p-1'
-        type='password'
-        id='password'
-        name='password'
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
-      {formik.touched.password && formik.errors.password ? (
-        <div>{formik.errors.password}</div>
-      ) : null}
-      <button type='submit'>Register</button>
-    </form>
+        <label htmlFor='password' className='font-bold mt-4'>
+          Password:
+        </label>
+        <input
+          className='border text-primary border-gray-400 rounded p-1'
+          type='password'
+          id='password'
+          name='password'
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div className='text-orange'>{formik.errors.password}</div>
+        ) : null}
+
+        <label htmlFor='avatar' className='relative float-left font-bold mt-4'>
+          Avatar URL (optional):
+        </label>
+        <input
+          className='border text-primary border-gray-400 rounded p-1'
+          type='url'
+          name='avatar'
+          id='avatar'
+          placeholder='https://example.com'
+          pattern='https://.*'
+          size='30'
+          onChange={formik.handleChange}
+          value={formik.values.url}
+        />
+        {formik.touched.avatar && formik.errors.avatar ? (
+          <div className='text-orange'>{formik.errors.avatar}</div>
+        ) : null}
+
+        <div className='mb-6 flex min-h-[1.5rem] items-center justify-center pl-[1.5rem]'>
+          <input
+            className="relative float-left -ml-[1.5rem] mr-[6px] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent "
+            type='checkbox'
+            onChange={formik.handleChange}
+            value={formik.values.venueManager}
+            id='venueManager'
+          />
+          <label
+            className='inline-block pl-[0.15rem] hover:cursor-pointer'
+            htmlFor='venueManager'
+          >
+            Register as Venue Manager
+          </label>
+        </div>
+
+        <button type='submit' className='btnSecondary bg-orange mt-6'>
+          Register
+        </button>
+      </form>
+    </div>
   );
 };
 
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,9 +1,26 @@
+import { Link } from 'react-router-dom';
 import RegisterForm from '../../components/RegisterForm';
 
 const Register = () => {
   return (
-    <div className='md:bg-secondary px-12 p-4 max-w-screen-2xl mx-auto mt-24'>
-      <RegisterForm />
+    <div className='bg-yellow px-12 p-4 max-w-screen-2xl mx-auto mt-24'>
+      <div className='text-center mt-8 flex flex-col items-center'>
+        <h2 className='md:text-4xl text-2xl font-bold fontPrimary text-primary mb-8'>
+          Sign Up
+        </h2>
+        <div className='text-secondary'>
+          <RegisterForm />
+        </div>
+        <p className='mt-6 text-center text-primary mb-8'>
+          Already have an account?{' '}
+          <Link
+            to='/login'
+            className='text-green hover:text-green-600 dark:text-green-400 dark:hover:text-green-300 font-bold transition-all duration-300 ease-in-out'
+          >
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
